f5738da4434a6c5938f7c3a8f10f70d076690bf5
Cristian Pallarés <cristian@pallares.io>
2018-05-08 17:05:56 +0200

    test: test JSX components

--- tests/vue.spec.js
+++ tests/vue.spec.js
@@ -1,11 +1,21 @@
 const { createEngine, cleanReport } = require("./utils");
 
-test("should validate without errors", () => {
+test("should validate SFC without errors", () => {
     const engine = createEngine({
         configFile: require.resolve("../vue.js"),
     });
 
     const report = engine.executeOnFiles(["component.fixture.vue"]);
+
+    expect(cleanReport(report)).toMatchSnapshot();
+});
+
+test("should validate JSX without errors", () => {
+    const engine = createEngine({
+        configFile: require.resolve("../vue.js"),
+    });
+
+    const report = engine.executeOnFiles(["component.fixture.jsx"]);
 
     expect(cleanReport(report)).toMatchSnapshot();
 });
